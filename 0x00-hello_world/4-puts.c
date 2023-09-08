4349d3bf9a3bf1ad022cd9e701fc98632cdd41c6
Jude Nnadi <judennadi76@gmail.com>
2023-09-07 18:26:37 -0700

    Added a new File

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints the inputed string using the 'put' function.
- * This program prints the string; 'Programming is like building a multilingual puzzle'
+ * This program prints the string \"Programming is like building a multilingual puzzle
+ * Return : 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
