b1c737e622d9ffe954937d2b0ddbc443961bfe90
Jude Nnadi <judennadi76@gmail.com>
2023-09-07 18:47:40 -0700

    Added a new File

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - prints the inputed string using the 'printf' function.
+* This program prints the string: 'with proper grammar, but the outcome is a piece of art,'
+* Instructions: Don't use puts function 
+* Return : 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
