38205cd248252675ddbb74714ab2e8ec29ae4f28
Jude Nnadi <judennadi76@gmail.com>
2023-09-09 15:45:33 -0700

    Added a new file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - prints all the numbers of base 16 in lowercas.
+* a program that prints all the numbers of base 16 in lowercas and a new lie.
+* Return: 0
+*/
+
+int main(void)
+{
+	int dec;
+	char hex;
+
+	for (dec = 0; dec <= 26; dec++)
+	{
+		putchar(dec);
+	}
+	for (hex = 'a'; hex <= 'z'; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+
+	return (0);
+}
