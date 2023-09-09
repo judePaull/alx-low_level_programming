a8156b5650ff7421ad99f5b2ed225e350dde2e8a
Jude Nnadi <judennadi76@gmail.com>
2023-09-09 14:52:24 -0700

    Added a new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints the alphabet in lower-case.
+* a program that prints the alphabet in lowercase, followed by a new line.
+* Return: 0
+*/
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+
+	putchar('\n');
+	return (0);
+}
