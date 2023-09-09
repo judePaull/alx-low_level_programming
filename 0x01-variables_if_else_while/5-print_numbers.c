2a900390e4aed70901fad7c63dfb99a30301eb31
Jude Nnadi <judennadi76@gmail.com>
2023-09-09 15:21:55 -0700

    Added a new file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints single digit numbers of base ten.
+* a program single digit numbers of base 10, followed by a new lie.
+* Return: 0
+*/
+
+int main(void)
+{
+	int sing_digit;
+
+	for (sing_digit = '0'; sing_digit <= 9; sing_digit++)
+	{
+		putchar(sing_digit);
+	}
+	putchar('\n');
+
+	return (0);
+}
