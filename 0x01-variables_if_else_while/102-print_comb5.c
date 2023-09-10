9e234ee0dce724c7c2eebabcdbe56f4b05e5fd7c
Jude Nnadi <judennadi76@gmail.com>
2023-09-09 17:13:35 -0700

    Added a new file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible combinations of two two-digit numbers.
+* Return: 0
+*/
+
+int main(void)
+{
+	int j;
+	int n;
+
+	for (j = 0; j <= 99; j++)
+	{
+		for (n = j + 1; n <= 99; n++)
+		{
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			putchar(' ');
+			putchar(n / 10 + '0');
+			putchar(n % 10 + '0');
+			if (j != 99 || n != 99)
+			{
+			putchar(',');
+		putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
