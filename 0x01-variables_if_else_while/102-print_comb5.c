4b8cc54b0461e27cfb0a66b4760fde54f62dc95f
Jude Nnadi <judennadi76@gmail.com>
2023-09-09 17:28:00 -0700

    Added a new file

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,26 +7,36 @@
 
 int main(void)
 {
-	int j;
-	int n;
-
-	for (j = 0; j <= 99; j++)
+int c, i, k, j;
+for (c = 48; c <= 57; c++)
+{
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = 0; n <= 99; n++)
+		for (k = 48; k <= 57; k++)
 		{
-			putchar(j / 10 + '0');
-			putchar(j % 10 + '0');
-			putchar(' ');
-			putchar(n / 10 + '0');
-			putchar(n % 10 + '0');
-			if (j != 99 || n != 99)
+			for (j = 48; j <= 57; j++)
 			{
-			putchar(',');
-		putchar(' ');
+				if (((k + j) > (c + i) &&  k >= c) || c < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(' ');
+					putchar(k);
+					putchar(j);
+				if (c + i + k + j == 227 && c == 57)
+				{
+				break;
+				}
+				else
+				{
+				putchar(',');
+				putchar(' ');
+				}
+				}
 			}
 		}
 	}
-	putchar('\n');
-
-	return (0);
+}
+putchar('\n');
+return (0);
 }
