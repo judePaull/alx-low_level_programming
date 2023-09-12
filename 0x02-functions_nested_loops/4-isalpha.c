d3e5273045cae41027ba5c1e26ccde022ed6eaf7
Jude Nnadi <judennadi76@gmail.com>
2023-09-12 14:43:58 -0700

    Added a new file

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _isalpha - Point of Entry.
+* @c : inputed character to be checked.
+* Return: 0
+*/
+
+int _isalpha(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
