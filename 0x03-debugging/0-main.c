066b38c87f62a44d00032e2986c7b487afe07e7d
Jude Nnadi <judennadi76@gmail.com>
2023-09-14 19:18:38 -0700

    Added a new file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,15 @@
  * main - Test function for positive or negative
  * Return: 0
  */
+
+void positive_or_negative(int i);
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-	
-	printf("Testing positive_or_negative() with 0:\n");
+
 	positive_or_negative(i);
 
 	return (0);
