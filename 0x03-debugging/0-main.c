07be7239af97f7e41f76c3644e72797519639440
Jude Nnadi <judennadi76@gmail.com>
2023-09-14 18:52:48 -0700

    Added a new file

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - program to test a function for positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,7 @@
+# 0x03-debugging
+
+- A directory containing *4 Tasks* that entails the task  C - Debugging. 
+
+## Features
+
+- *4 executable files*.
