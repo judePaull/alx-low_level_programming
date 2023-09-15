32184e3b413a1257cd1c88446b335fbb0e368274
Jude Nnadi <judennadi76@gmail.com>
2023-09-15 13:41:02 -0700

    Added a new file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case letters
+ * @c : character to be checked
+ * Return: 0 or 1
+ */
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+# 0x04-more_functions_nested_loops
+
+- A directory containing *13 Tasks* that entails the task C - More functions, more nested loops
+C. 
+
+## Features
+
+- *13 executable files*.
