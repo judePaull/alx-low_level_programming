b9f29a3538b6fd1dbe1d51a1281849b84bc362f6
Jude Nnadi <judennadi76@gmail.com>
2023-09-15 18:11:38 -0700

    Added a new file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - prints # as a triangle according to digit inputted.
+ * @size : number of n to be printed
+ * Return:void
+ */
+
+void print_triangle(int size)
+{
+	int l = 1, ll;
+
+	while (l < size && size > 0)
+	{
+		ll = 0;
+		while (ll < size - l)
+		{
+			_putchar(' ');
+			ll++;
+		}
+		ll = 0;
+		while (ll < l)
+		{
+			_putchar('#');
+				ll++;
+		}
+		_putchar('\n');
+		l++;
+	}
+	if (l == 1)
+		_putchar('\n');
+}
