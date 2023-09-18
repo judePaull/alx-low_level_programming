05f2732dcb9ede75b854f61d332699121570fee0
Jude Nnadi <judennadi76@gmail.com>
2023-09-18 15:35:11 -0700

    added a new file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of an int to 98
+ * @n : pointer that updates value
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+# 0x05-pointers_arrays_strings
+
+- A directory containing *12 Tasks* that entails the task C - Pointers, arrays and strings. 
+
+## Features
+
+- *12 executable files*.
