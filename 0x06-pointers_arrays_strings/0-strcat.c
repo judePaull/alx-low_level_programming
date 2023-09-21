074be6d8d8104cfe78be53f4b129fbc08b6eb920
Jude Nnadi <judennadi76@gmail.com>
2023-09-21 02:25:12 -0700

    added a new file

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - ppends the src string to the dest string
+ * @dest : string to be appended.
+ * @src : string to append.
+ * Return : Always 0.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+ # 0x06-pointers_arrays_strings
+
+- A directory containing *13 Tasks* that entails the task C - More pointers, arrays and strings. 
+
+## Features
+
+- *13 executable files*.
