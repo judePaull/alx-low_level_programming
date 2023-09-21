a87bdac0b42916256b05a71d8d48b14a9996a872
Jude Nnadi <judennadi76@gmail.com>
2023-09-21 08:59:17 -0700

    added a new file

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ * _strcat - appends the src string to the dest string
+ * @dest: array  with string to be appended.
+ * @src: pointer where text is to append.
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int l;
+	int d;
+
+	for (l = 0; dest[l] != '\0'; l++)
+		;
+	for (d = 0; d < n && src[d] != '\0'; d++)
+		dest[l + d] = src[d];
+
+	dest[l + d] = '\0';
+
+	return (dest);
+}
