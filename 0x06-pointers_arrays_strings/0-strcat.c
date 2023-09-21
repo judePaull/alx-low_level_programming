f1587639331bf8fb5625f331651a34fbe1aa9843
Jude Nnadi <judennadi76@gmail.com>
2023-09-21 08:09:28 -0700

    Added a new file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,5 +10,20 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int d = 0;
+	int l = 0;
 
+	while (dest[d] != '\0')
+	{
+		d++;
+	}
+	while (src[l] != '\0')
+	{
+		dest[d] = src[l];
+		d++;
+		l++;
+	}
+	dest[d] = '\0';
+
+	return dest;
 }
